32b28945916d4d7beaf0ef5ac5adbe82871d42bb
anderson <anderson361163@gmail.com>
2025-01-28 00:31:43 -0300

    feat: Adicionando um botao de sair temporario

--- mobilehackaton/view/aluno/AlunoInicio.tsx
+++ mobilehackaton/view/aluno/AlunoInicio.tsx
@@ -28,6 +28,9 @@ export default function AlunoInicio() {
       <TouchableOpacity style={styles.button} onPress={()=>{linkTo('/RegistroPresenca')}}>
         <Text style={styles.buttonText} >Check-in</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={()=>{linkTo('/Login')}}>
+        <Text style={styles.buttonText} >Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 }
